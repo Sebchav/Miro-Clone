05ab58b8abae3c0020c195eb37a8012408263720
Sebchav <schavarria13@alumnos.uaq.mx>
2024-04-17 14:44:32 -0600

    Organizations, Tooltip and Sidebar finished

---
+++ components/hint.tsx
@@ -0,0 +1,47 @@
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger
+} from "@/components/ui/tooltip";
+
+export interface HintProps {
+    children: React.ReactNode;
+    label: string;
+    side?: "top" | "right" | "bottom" | "left";
+    align?: "start" | "center" | "end";
+    sideOffset?: number;
+    alignOffset?: number;
+}
+
+const Hint = ({
+    label,
+    children,
+    side,
+    align,
+    sideOffset,
+    alignOffset
+}: HintProps) => {
+  return (
+    <TooltipProvider>
+        <Tooltip delayDuration={100}>
+            <TooltipTrigger asChild>
+                {children}
+            </TooltipTrigger>
+            <TooltipContent
+                className="text-white bg-black border-black"
+                side={side}
+                align={align}
+                sideOffset={sideOffset}
+                alignOffset={alignOffset}
+            >
+                <p className="font-semibold capitalize">
+                    {label}
+                </p>
+            </TooltipContent>
+        </Tooltip>
+    </TooltipProvider>
+  )
+}
+
+export default Hint
