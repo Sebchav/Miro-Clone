27c0c81e88e785c9c4f6864c4d179df3b8f4e619
Sebchav <schavarria13@alumnos.uaq.mx>
2024-04-25 23:58:10 -0600

    New Board Button added

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -3,6 +3,7 @@
 import { useQuery } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 import BoardCard from './board';
+import NewBoardButton from './new-board-buttons';
 
 interface BoardListProps {
     orgId: string;
@@ -51,6 +52,7 @@ const BoardList = ({ orgId, query }:BoardListProps) => {
       {query.favorites ? "Favorite Boards" : "Team Boards"}
      </h2>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10'>
+        <NewBoardButton orgId={orgId}/>
         {data?.map((board)=> (
           <BoardCard 
             key={board._id}
