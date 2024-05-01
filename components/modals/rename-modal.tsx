54fa66862f45a0ad5bbb89ccc92a47f7e2638e6c
Sebchav <schavarria13@alumnos.uaq.mx>
2024-04-30 23:16:03 -0600

    Card actions finished

--- components/modals/rename-modal.tsx
+++ components/modals/rename-modal.tsx
@@ -6,19 +6,51 @@ import {
    DialogDescription,
    DialogHeader,
    DialogClose, 
-   DialogTitle 
+   DialogTitle, 
+   DialogFooter
 } from "@/components/ui/dialog";
 import { useRenameModal } from "@/store/use-rename-modal";
 
-import React from 'react'
+import React, { FormEvent, FormEventHandler, useEffect, useState } from 'react'
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 const RenameModal = () => {
+
+    const { mutate, pending } = useApiMutation(api.board.update);
 
     const {
         isOpen,
         onClose,
         initialValues
     } = useRenameModal();
+
+    const [ title , setTitle ] = useState(initialValues.title);
+
+    useEffect(() => {
+        setTitle(initialValues.title);
+    }, [initialValues.title]);
+
+    const onSubmit: FormEventHandler<HTMLFormElement> = (
+        e
+    ) => {
+        e.preventDefault();
+        mutate({
+            id: initialValues.id,
+            title
+        })
+            .then(() => {
+                toast.success("Board renamed");
+                onClose();
+            })
+            .catch(() => {
+                toast.error("Failed to rename board");
+                onClose();
+        })
+    }
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
@@ -31,6 +63,29 @@ const RenameModal = () => {
             <DialogDescription>
                 Enter a new title for this board
             </DialogDescription>
+            <form
+                onSubmit={onSubmit}
+                className="space-y-4"
+            >
+                <Input 
+                    disabled={pending}
+                    required
+                    maxLength={60}
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    placeholder="Board title"
+                />
+                <DialogFooter>
+                    <DialogClose>
+                        <Button type="button" variant={"outline"}>
+                            Cancel
+                        </Button>
+                    </DialogClose>
+                    <Button disabled={pending} type="submit">
+                        Save 
+                    </Button>
+                </DialogFooter>
+            </form>
         </DialogContent>
     </Dialog>
   )
