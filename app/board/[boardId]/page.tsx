db17945e7f5181e8f2a66a7c9bc586380aedc5b9
Sebchav <schavarria13@alumnos.uaq.mx>
2024-05-02 23:56:57 -0600

    Liveblocks setup

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Canvas from './_components/canvas'
+import Room from '@/components/room'
+import Loading from './_components/loading'
 
 interface BoardIdPageProps {
     params: {
@@ -10,8 +12,11 @@ interface BoardIdPageProps {
 const BoardIdPage = ({
     params
 }: BoardIdPageProps) => {
+
   return (
-    <Canvas boardId={params.boardId}/>
+    <Room roomId={params.boardId} fallback={<Loading />}>
+      <Canvas boardId={params.boardId}/>
+    </Room>
   )
 }
 
